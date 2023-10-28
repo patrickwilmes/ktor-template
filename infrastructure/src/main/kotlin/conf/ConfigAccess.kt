4db83ac51143b2c59596c8fa4cc3097a2b5429dc
Patrick Wilmes <p.wilmes89@gmail.com>
2023-10-28 11:33:09 +0200

    Removing unused config access classes
    The given config access classes where not used. They could be utilized when cloning the template, but they had very basic code in them, that's not worth keeping just because we might need it when using the template.

--- infrastructure/src/main/kotlin/conf/ConfigAccess.kt
+++
@@ -1,8 +0,0 @@
-package conf
-
-import com.typesafe.config.ConfigFactory
-import io.ktor.server.config.*
-
-object ConfigAccess {
-    private val appConfig = HoconApplicationConfig(ConfigFactory.load())
-}
